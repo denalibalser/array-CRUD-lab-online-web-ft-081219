4abd1ae3df16f7bd0294b03c55e4ff29c85e8165
Denali Balser <denali.balser167@gmail.com>
2019-08-14 16:12:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,17 +7,17 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1, 2, 3, 4]
+  #array = [1, 2, 3, 4]
   array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [1, 2, 3, 4]
+  #array = [1, 2, 3, 4]
   array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = [1, 2, 3, 4]
+  array
   array.pop
 end
 
